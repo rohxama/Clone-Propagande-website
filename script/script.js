58e03b05d7c5127e643f955e0b6cd13a33fa037b
rohxama <rohxama@gmail.com>
2025-10-10 15:59:48 +0500

    Adding locomotive for smooth scrolling

--- script/script.js
+++ script/script.js
@@ -9,33 +9,47 @@ window.addEventListener("load", () => {
   });
 });
 
-// Continuous vertical scrolling for
-const info = document.querySelector(".info");
-const items = gsap.utils.toArray(".item");
+// Smooth scrolling and infinite scroll effect
+document.addEventListener("DOMContentLoaded", () => {
+  const info = document.querySelector(".info");
+  const items = gsap.utils.toArray(".item");
 
-info.appendChild(items[0].cloneNode(true));
+  info.appendChild(items[0].cloneNode(true));
 
-const scrollAnim = gsap.to(items, {
-  yPercent: -100 * items.length,
-  ease: "none",
-  duration: 80,
-  repeat: -1,
-  modifiers: {
-    yPercent: gsap.utils.wrap(-100 * items.length, 0),
-  },
-});
+  const locoScroll = new LocomotiveScroll({
+    el: info,
+    smooth: true,
+    lerp: 0.07, 
+    multiplier: 1,
+    touchMultiplier: 2,
+    tablet: { smooth: true },
+    smartphone: { smooth: true },
+  });
 
-info.addEventListener("mouseenter", () => scrollAnim.pause());
-info.addEventListener("mouseleave", () => scrollAnim.play());
+  const scrollAnim = gsap.to(items, {
+    yPercent: -100 * items.length,
+    ease: "none",
+    duration: 80,
+    repeat: -1,
+    modifiers: {
+      yPercent: gsap.utils.wrap(-100 * items.length, 0),
+    },
+  });
 
-info.addEventListener("scroll", () => {
-  if (info.scrollTop >= info.scrollHeight - info.clientHeight) {
-    info.scrollTop = 1;
-  } else if (info.scrollTop <= 0) {
-    info.scrollTop = info.scrollHeight - info.clientHeight - 1;
-  }
-});
+  // pause on hover
+  info.addEventListener("mouseenter", () => scrollAnim.pause());
+  info.addEventListener("mouseleave", () => scrollAnim.play());
 
-window.addEventListener("resize", () => {
-  scrollAnim.invalidate();
+  info.addEventListener("scroll", () => {
+    if (info.scrollTop >= info.scrollHeight - info.clientHeight) {
+      info.scrollTop = 1;
+    } else if (info.scrollTop <= 0) {
+      info.scrollTop = info.scrollHeight - info.clientHeight - 1;
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    scrollAnim.invalidate();
+    locoScroll.update();
+  });
 });
